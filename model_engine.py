f37fff60470e2f7a516c4ccf59c7d8b031e870ff
nealjackson <neal.jackson@manchester.ac.uk>
2017-03-19 19:43:58 +0000

    Add files via upload

--- model_engine.py
+++ model_engine.py
@@ -2,18 +2,14 @@
 #    from u-v data on a particular closure triangle
 #           v.1 Neal Jackson, 2015.09.29
 #           v.2 NJ, 2017.01.16 converted to CASA, many changes
-import astropy,numpy as np,scipy,sys,os,glob,warnings,pyrap,multiprocessing
-from pyrap import tables as pt
-from astropy.coordinates import SkyCoord
-from scipy import optimize,ndimage; from scipy.ndimage import measurements
-import matplotlib; from matplotlib import patches,pyplot as plt
-from correlate import *; from mkgauss import *
-plt.rcParams['image.origin']='lower'
-plt.rcParams['image.interpolation']='nearest'
+#           v.3 NJ, 2017.03.07 parallelised
+import astropy,numpy as np,scipy,sys,os,glob,warnings,pyrap,multiprocessing,matplotlib
+from pyrap import tables as pt; from matplotlib import pyplot as plt
+from scipy import ndimage,optimize; from correlate import *; from mkgauss import *
+plt.rcParams['image.origin'],plt.rcParams['image.interpolation']='lower','nearest'
 warnings.simplefilter('ignore')
-RAD2ARC,LIGHT = 3600.*180./np.pi, 2.99792458E+8
+RAD2ARC,LIGHT,FIRSTNPY = 3600.*180./np.pi, 2.99792458E+8, './first_2008.simple.npy'
 MX,MY,MF,MW,MR,MP = range(6)
-FIRSTNPY = './first_2008.simple.npy'
 
 # Make a movie from a set of png files
 def movie (fps=4):
@@ -75,14 +71,11 @@ def squash_amp_clph (d,spw,nspw,pol=0):
         idx = i/nspw
         a[idx] += np.nansum(d[i]['DATA'][:,pol])
         n[idx] += len(~np.isnan(d[0]['DATA'][:,pol]))
-    A = abs(a/n)
-    P = np.arctan2((a/n).imag,(a/n).real)
-    np.putmask(P,P>np.pi,P-2.*np.pi)
-    np.putmask(P,P<-np.pi,P+2.*np.pi)
-    return A,P
+    return abs(a/n),norm(np.arctan2((a/n).imag,(a/n).real),False,False)
 
 # Return two amplitude arrays and a closure phase array from 3 data arrays
 def get_amp_clph (d01,d02,d12,spw,otel,nspw=0,pol=0):
+    global cp012,a01,a02,a12
     sp = np.array([])
     for i in range(len(d01)):
         sp = np.append(sp, spw[i]['DATA_DESC_ID'])
@@ -90,10 +83,9 @@ def get_amp_clph (d01,d02,d12,spw,otel,nspw=0,pol=0):
     a01,p01 = squash_amp_clph (d01,spw, nspw)
     a02,p02 = squash_amp_clph (d02,spw, nspw)
     a12,p12 = squash_amp_clph (d12,spw, nspw)
-    cp = p01*otel[0]-p02*otel[1]+p12*otel[2]
-    np.putmask(cp,cp>np.pi,cp-2.*np.pi)
-    np.putmask(cp,cp<-np.pi,cp+2.*np.pi)
-    return np.rad2deg(cp),a01,a02,a12
+    cp012 = np.rad2deg(p01*otel[0]-p02*otel[1]+p12*otel[2])
+    np.putmask(cp012,cp012>180.0,cp012-360.0)
+    np.putmask(cp012,cp012<-180.0,cp012+360.0)
 
 def get_uvw_table (t):
     for u in t.select('UVW'):
@@ -104,9 +96,10 @@ def get_uvw_table (t):
     return uvw
 
 # Get data and u-v arrays from a measurement set on a given triangle
-def data_extract (data,trname):
-    os.system ('msoverview in='+data+' >temp_msoverview')
-    os.system('cat temp_msoverview')
+def data_extract (data):
+    global uvw01,uvw02,uvw12
+    ctemplate = 'taql \'select from %s where ANTENNA1==%d and ANTENNA2==%d giving cl_tmp%d.ms\''
+    os.system ('msoverview in='+data+' >temp_msoverview;cat temp_msoverview')
     f = open('temp_msoverview')
     nif = 0
     for l in f:    # parse msoverview output to get channel information
@@ -130,19 +123,13 @@ def data_extract (data,trname):
     otel = 1.-2.*np.asarray([itel[0]>itel[1],itel[0]>itel[2],itel[1]>itel[2]],dtype=float)
     btel = [min(itel[0],itel[1]),max(itel[0],itel[1]),min(itel[0],itel[2]),\
             max(itel[0],itel[2]),min(itel[1],itel[2]),max(itel[1],itel[2])]
-    os.system('rm -fr closure_temp*.ms')
-    command = 'taql \'select from %s where ANTENNA1==%d and ANTENNA2==%d giving closure_temp1.ms\'' %(data,btel[0],btel[1])
-    os.system(command)
-    command = 'taql \'select from %s where ANTENNA1==%d and ANTENNA2==%d giving closure_temp2.ms\'' %(data,btel[2],btel[3])
-    os.system(command)
-    command = 'taql \'select from %s where ANTENNA1==%d and ANTENNA2==%d giving closure_temp3.ms\'' %(data,btel[4],btel[5])
-    os.system(command)
-    t01 = pt.table('closure_temp1.ms')
-    t02 = pt.table('closure_temp2.ms')
-    t12 = pt.table('closure_temp3.ms')
+    os.system (ctemplate % (data,btel[0],btel[1],1))
+    os.system (ctemplate % (data,btel[2],btel[3],2))
+    os.system (ctemplate % (data,btel[4],btel[5],3))
+    t01,t02,t12 = pt.table('cl_tmp1.ms'),pt.table('cl_tmp2.ms'),pt.table('cl_tmp3.ms')
     spw = t01.select('DATA_DESC_ID')
     d01,d02,d12 = t01.select('DATA'), t02.select('DATA'), t12.select('DATA')
-    cp012,a01,a02,a12 = get_amp_clph(d01,d02,d12,spw,otel)
+    get_amp_clph(d01,d02,d12,spw,otel)
     times = np.array([])
     for t in t01.select('TIME'):
         times = np.append(times,t['TIME'])
@@ -150,17 +137,16 @@ def data_extract (data,trname):
     uvw02 = get_uvw_table(t02)/wlength
     uvw12 = get_uvw_table(t12)/wlength
     print trname,'-> antenna numbers:',itel
-    bl01 = np.sqrt(uvw01[0,0]**2+uvw01[0,1]**2+uvw01[0,2]**2)
-    bl02 = np.sqrt(uvw02[0,0]**2+uvw02[0,1]**2+uvw02[0,2]**2)
-    bl12 = np.sqrt(uvw12[0,0]**2+uvw12[0,1]**2+uvw12[0,2]**2)
     print 'Baseline lengths: %s-%s: %dkm %s-%s: %dkm %s-%s: %dkm' % \
-       (trname[0],trname[1],int(bl01*wlength/1000),trname[0],trname[2],\
-        int(bl02*wlength/1000),trname[1],trname[2],int(bl12*wlength/1000))
-    return a01,a02,cp012,uvw01,uvw02,uvw12,itel,np.mean(wlength),ra,dec
+       (trname[0],trname[1],int(np.sqrt((uvw01[0]**2).sum())*wlength/1000),\
+        trname[0],trname[2],int(np.sqrt((uvw02[0]**2).sum())*wlength/1000),\
+        trname[1],trname[2],int(np.sqrt((uvw12[0]**2).sum())*wlength/1000))
+    os.system('rm -fr cl_tmp*.ms')
+    return itel,np.mean(wlength),ra,dec
 
 # --------------------------------------------------
 
-def model_extract (model,uvw01,uvw02,uvw12,itel):
+def model_extract (model,itel):
     otel = 1.-2.*np.asarray([itel[0]>itel[1],itel[0]>itel[2],itel[1]>itel[2]],dtype=float)
     re01,im01 = uvw2reim (uvw01,model)
     re12,im12 = uvw2reim (uvw12,model)
@@ -171,15 +157,14 @@ def model_extract (model,uvw01,uvw02,uvw12,itel):
     clph = norm(ph01*otel[0] - ph02*otel[1] + ph12*otel[2])
     return np.hypot(re01,im01),np.hypot(re02,im02),clph
 
-def plotimg (a01,a02,A01,A02,cp012,CP012,model,goodness,plottype,itel,aplot,trname,glim):
-    plotnum = len(glob.glob('model_engine_*.png'))
+def plotimg (A01,A02,CP012,model,goodness,itel,aplot,gcou):
     ells = []
     for i in model:
         if i[MW]!=0.0:
-            ells.append(patches.Ellipse(xy=[i[MX],i[MY]],width=2.*i[MW],\
+            ells.append(matplotlib.patches.Ellipse(xy=[i[MX],i[MY]],width=2.*i[MW],\
                 height=2.*i[MW]*i[MR],lw=0.5,angle=i[MP]+90.,fill=0.0))
         else:
-            ells.append(patches.Ellipse(xy=[i[MX],i[MY]],width=0.1,\
+            ells.append(matplotlib.patches.Ellipse(xy=[i[MX],i[MY]],width=0.1,\
                 height=0.1,lw=0.5,angle=0.,fill=1.0,color='red'))
     if plottype in [1,10]:
         plt.subplot2grid((6,5),(0,0),rowspan=2)
@@ -228,11 +213,12 @@ def plotimg (a01,a02,A01,A02,cp012,CP012,model,goodness,plottype,itel,aplot,trna
         e.set_clip_box(ax.bbox)
     ax.set_xlim(-glim,glim);ax.set_ylim(-glim,glim)
     plt.grid()
-    plt.title('model_engine_%03d.png'%plotnum)
+    if gcou==-1:   # find how many png files, make the next one
+        gcou = len(glob.glob('model_engine_*.png'))
+    plt.title('model_engine_%03d.png'%gcou)
     if plottype in [10,20]:
-        plt.savefig('model_engine_%03d.png'%plotnum)
+        plt.savefig('model_engine_%03d.png'%gcou)
 
-    
 #===================================================
 def ndiff (a,b):
     sqd = np.array([])
@@ -244,7 +230,7 @@ def ndiff (a,b):
 
 # this is the bit that may need replacing - maybe fit splines and look at the spline points?
 
-def get_goodness(a01,a02,cp012,A01,A02,CP012,plotnum):
+def get_goodness(A01,A02,CP012):
     beta = 0.00001    #   this is a pretty vital parameter
     ascat = np.median(abs(np.gradient(np.ravel(a02))))    
     cscat = np.median(abs(np.gradient(np.ravel(cp012))))
@@ -255,25 +241,24 @@ def get_goodness(a01,a02,cp012,A01,A02,CP012,plotnum):
     return sq.min() + beta*difmin**2
 
 def mod_func (x0, *x):
-    plotnum = len(glob.glob('model_engine_*.png'))
-    model,opt,uvw01,uvw02,uvw12,a01,a02,cp012,ampfiddle,plottype,itel,aplot,trname,glim = x
+    model,opt,itel,aplot,gcou,iy,ix = x
     model,opt = np.ravel(model),np.ravel(opt)
     model[opt] = x0
     model = model.reshape(len(model)/6,6)
-    A01,A02,CP012 = model_extract (model,uvw01,uvw02,uvw12,itel)
+    A01,A02,CP012 = model_extract (model,itel)
     if ampfiddle:
         A01 *= np.median(a01)/np.median(A01)
         A02 *= np.median(a02)/np.median(A02)
-    goodness = get_goodness(a01,a02,cp012,A01,A02,CP012,plotnum)
+    goodness = get_goodness(A01,A02,CP012)
     if plottype:
-        plotimg (a01,a02,A01,A02,cp012,CP012,model,goodness,plottype,itel,aplot,trname,glim)
+        plotimg (A01,A02,CP012,model,goodness,itel,aplot,gcou)
         if plottype in [1,2]:
             plt.draw()
             plt.pause(0.001)
             plt.clf()
     return goodness
 
-def getmodel(coord,beam,bsub,gridsize):
+def getmodel(coord,beam):
     gsiz = int(gridsize/(bsub*beam))   # original grid very big
     ginc = bsub*beam                   # arcsec/pix grid size
     grid = np.ones((gsiz,gsiz))*np.nan # original grid full of NaN
@@ -284,7 +269,7 @@ def getmodel(coord,beam,bsub,gridsize):
     print 'Found: %d FIRST sources'%len(a)
     for i in range(len(a)):
         fi = first[int(a[i,1])]
-        scoord = SkyCoord(fi[0],fi[1],unit='degree')
+        scoord = astropy.coordinates.SkyCoord(fi[0],fi[1],unit='degree')
         scoord = scoord.to_string(style='hmsdms')
         scoord = scoord.replace('d',':').replace('m',':')
         scoord = scoord.replace('h',':').replace('s','')
@@ -310,8 +295,6 @@ def parallel_function(f):
         from multiprocessing import Pool
         ncores = max(1,int(os.popen('nproc').read())-1)  # use all cores - 1
         pool = Pool(processes=ncores) # depends on available cores
-        print f
-        print sequence
         result = pool.map(f, sequence) # for i in sequence: result[i] = f(i)
         cleaned = [x for x in result if not x is None] # getting results
         cleaned = np.asarray(cleaned)
@@ -322,33 +305,33 @@ def parallel_function(f):
     return partial(easy_parallize, f)
 
 def grid_search_thread (k):
-    return mod_func(k[0],k[1],k[2],k[3],k[4],k[5],k[6],k[7],k[8],\
-                    k[9],k[10],k[11],k[12],k[13],k[14])
+    a =  mod_func(k[0],k[1],k[2],k[3],k[4],k[5],k[6],k[7])
+    f=open('grid_search_thread.log','a')
+    f.write('%d %d %f\n'%(k[-2],k[-1],a))
+    f.close()
+    return a
 
-def grid_search (model,cpt,gridcpt,uvw01,uvw02,uvw12,a01,a02,cp012,ampfiddle,\
-                 plottype,itel,aplot,trname,grid,gsiz,ginc,glim):
+def grid_search (model,cpt,gridcpt,itel,aplot,gcou,grid,gsiz,ginc):
     opt = np.zeros_like(model,dtype='bool')
     args = []
+    import copy
     for ix in range(gsiz):
         x = ginc*(ix-gsiz/2.0)   # x,y in arcsec; a in ginc-size pixels
         for iy in range(gsiz):
             if grid[iy,ix] == gridcpt:
                 y = ginc*(iy-gsiz/2.0)
                 model[cpt][0],model[cpt][1] = x,y
-#                aplot[iy][ix] = mod_func ([],model,opt,uvw01,uvw02,uvw12,\
-#                    a01,a02,cp012,ampfiddle,plottype,itel,aplot,trname,glim)
-#  if arg array too big make uvw01....cp012 global
-                arg = ([],model,opt,uvw01,uvw02,uvw12,a01,a02,cp012,\
-                       ampfiddle,plottype,itel,aplot,trname,glim,iy,ix)
-                args.append(arg)
-#                print 'Grid point %d: (%.3f %.3f), chisq %.4f' %\
-#                    (ix*gsiz+iy,x,y,aplot[iy][ix])
+                arg = ([],model,opt,itel,aplot,gcou,iy,ix)
+                args.append(copy.deepcopy(arg))   # otherwise overwrites all elements
+                gcou += 1
+    print 'Starting grid search with',len(args),'points'
+    os.system('rm grid_search_thread.log')
     grid_search_thread.parallel = parallel_function(grid_search_thread)
     parallel_result = grid_search_thread.parallel (args)
     for i in range(len(parallel_result)):
         aplot[args[i][-2],args[i][-1]] = parallel_result[i]
     np.putmask(aplot,np.isnan(aplot),np.nanmax(aplot))
-    model[cpt,:2] = ginc*(np.asarray(measurements.minimum_position \
+    model[cpt,:2] = ginc*(np.asarray(ndimage.measurements.minimum_position \
             (aplot)[::-1])-0.5*np.asarray(grid.shape))
     return model,aplot
 
@@ -362,50 +345,64 @@ def recentroid (model,startcpt,endcpt,ginc,gsiz):
     model[startcpt:endcpt+1,:2] += np.array([0.5*gsiz,0.5*gsiz]) - centroid
     return model
 
-def adjust_all (model,uvw01,uvw02,uvw12,a01,a02,cp012,ampfiddle,plottype,itel,aplot,trname,glim):
+def adjust_all (model,itel,aplot,gcou):
     opt = np.ones_like(model,dtype='bool')
     x0 = np.ravel(model)[np.ravel(opt)]
-    args = (model,opt,uvw01,uvw02,uvw12,a01,a02,cp012,ampfiddle,plottype,itel,aplot,trname,glim)
+    args = (model,opt,itel,aplot,-1,0,0)
     xopt = optimize.fmin(mod_func, x0, args=args, maxiter=100)
     model,opt = np.ravel(model),np.ravel(opt)
     model[opt] = xopt
     model = model.reshape(len(model)/6,6)
     return model
 
-def refine_points (model,cpts,uvw01,uvw02,uvw12,a01,a02,cp012,ampfiddle,plottype,itel,aplot,trname,glim):
+def refine_points (model,cpts,itel,aplot,gcou):
     opt = np.zeros_like(model,dtype='bool')
     for i in cpts:
         opt[i,2:] = True
     x0 = np.ravel(model)[np.ravel(opt)]
-    args = (model,opt,uvw01,uvw02,uvw12,a01,a02,cp012,ampfiddle,plottype,itel,aplot,trname,glim)
-    xopt = optimize.fmin(mod_func, x0, args=args, maxiter=100)
+    args = (model,opt,itel,aplot,-1,0,0)
+    xopt = optimize.fmin(mod_func, x0, args=args, maxiter=20)
     model,opt = np.ravel(model),np.ravel(opt)
     model[opt] = xopt
     model = model.reshape(len(model)/6,6)
     return model
 
-
-def norm(a):
-    a = a%360.0
-    np.putmask(a,a>180.0,a-360.0)
-    np.putmask(a,a<-180.0,a+360.0)
+def norm(a,isdeg=True,isred=True):
+    nlim = 180.0 if isdeg else np.pi
+    a = a%(2*nlim) if isred else a
+    np.putmask(a,a>nlim,a-2.*nlim)
+    np.putmask(a,a<-nlim,a+2.*nlim)
     return a
 
+def write_skymodel (ra,dec,model,outname):
+    if outname!='':
+        f = open(outname,'w')
+        f.write ('# (Name, Type, Ra, Dec, I, MajorAxis, MinorAxis, Orientation) = format\n')
+    for i in range(len(model)):
+        cosd = 3600.*np.cos(np.deg2rad(dec))
+        s = astropy.coordinates.SkyCoord(ra-model[i,0]/cosd,dec+model[i,1]/3600,unit='degree')
+        s = s.to_string(style='hmsdms')
+        sra = s.split()[0].replace('h',':').replace('m',':').replace('s','')
+        sdec = s.split()[1].replace('d','.').replace('m','.').replace('s','')
+        print 'ME%d, GAUSSIAN, %s, %s, %f, %f, %f, %f'%(i,sra,sdec,model[i,MF],\
+              model[i,MW],model[i,MW]*model[i,MR],np.rad2deg(model[i,MP]))
+        if outname!='':
+            f.write('ME%d, GAUSSIAN, %s, %s, %f, %f, %f, %f\n'%(i,sra,sdec,model[i,MF],\
+                  model[i,MW],model[i,MW]*model[i,MR],np.rad2deg(model[i,MP])))
+    if outname!='':
+        f.close()
 
 #============= main script ========================
-def mainscript():
-    bsub,gridsize = 0.3,12.0    # fraction of beam for steps; default grid size
-    plottype,ampfiddle = 20, True
-#    data, trname = './SIM5.ms', ['ST001','DE601','DE605HBA']
-    data, trname = './PP1_av_corr.ms', ['ST001','DE601HBA','DE605HBA']
-#    ------ change things above this line ------
+def mainscript(data,TRNAME,BSUB=0.3,GRIDSIZE=12.0,PLOTTYPE=20,AMPFIDDLE=True,outname='model_engine.sky'):
+    global bsub,gridsize,plottype,ampfiddle,glim,trname
+    bsub,gridsize,plottype,ampfiddle,trname,gcou = BSUB,GRIDSIZE,PLOTTYPE,AMPFIDDLE,TRNAME,0
     os.system('rm model_engine*.png')
-    a01,a02,cp012,uvw01,uvw02,uvw12,itel,wv,ra,dec = data_extract (data,trname)
+    itel,wv,ra,dec = data_extract (data)
     s_amp = np.sort(np.ravel(a01)); ls = len(s_amp)
     flux1,flux2 = np.median(s_amp), 0.5*(s_amp[int(0.99*ls)]-s_amp[int(0.01*ls)])
     flux = np.median(a01)
     beam = RAD2ARC/np.nanmax(abs(uvw01)); print 'Beam:',beam,'arcsec'
-    grid,ginc,gsiz,pflux,pcoord = getmodel (np.array([ra,dec]),beam,bsub,gridsize)
+    grid,ginc,gsiz,pflux,pcoord = getmodel (np.array([ra,dec]),beam)
     if not len(pflux):   # no FIRST source, search the whole grid
         grid = np.zeros_like (grid)
     aplot = np.ones_like(grid)*np.nan
@@ -415,23 +412,27 @@ def mainscript():
         fig = plt.figure()
     if len(pflux) < 2:    # zero, or one FIRST source
         model = np.array([[0.0,0.0,flux1,0.5,1.0,0.0],[-1.0,-2.0,flux2,0.5,1.0,0.0]])
-        model,aplot = grid_search (model,1,0,uvw01,uvw02,uvw12,a01,a02,cp012,ampfiddle,plottype,itel,aplot,trname,grid,gsiz,ginc,glim)   
-# fix one cpt, grid-search posn of 2nd. aplot can be used to see how big the chisq advantage is of 2cpts
-        print 'Model after grid search',model
+        model,aplot = grid_search (model,1,0,itel,aplot,gcou,grid,gsiz,ginc)   
+# fix one cpt, grid-search posn of 2nd.
+        print 'Model after grid search:'
+        write_skymodel (ra,dec,model,'')
         model[0,MW] = model[1,MW] = 1.0
-        model = refine_points (model,[0,1],uvw01,uvw02,uvw12,a01,a02,cp012,ampfiddle,plottype,itel,aplot,trname,glim)  
+        model = refine_points (model,[0,1],itel,aplot,gcou)  
 # fit for size/orientation only
-        print 'Model after refining points',model
+        print 'Model after refining points:'
+        write_skymodel (ra,dec,model,'')
         model = recentroid (model,0,1,ginc,gsiz)  # put centroid in centre of image
-        print 'Model after recentroiding',model
+        print 'Model after recentroiding:'
     else:   # >1 FIRST source - not tested yet
         model = np.zeros((len(pflux),6))
         for i in range(len(pflux)):
             model[i,:2] = pcoord[i]
             model[i,3] = pflux[i]*flux/pflux.sum()
             model[i,4:] = [0.5,1.0,0.0]
-        model = adjust_all(model,data,uvw01,uvw02,uvw12,a01,a02,cp012,ampfiddle,plottype,itel,aplot,trname,glim)
+        model = adjust_all(model,itel,aplot,gcou)
     if plottype in [10,20]:
         movie()
-
-mainscript()
+    write_skymodel (ra,dec,model,'')
+    write_skymodel (ra,dec,model,outname)
+#mainscript('./SIM5.ms', ['ST001','DE601','DE605HBA'])
+mainscript('./PP1_av_corr.ms', ['ST001','DE601HBA','DE605HBA'])
