a9ca9f5adb623f5cd6a6e3935b9acf54ebbfa13c
nealjackson <neal.jackson@manchester.ac.uk>
2019-05-04 21:18:42 +0100

    Add files via upload

--- correlate.py
+++ correlate.py
@@ -1,12 +1,26 @@
 import numpy as np
+
+which = 0
 try:
     import astLib;from astLib import astCoords
-    have_astLib = True
+    which = 1
 except:
-    import astropy
-    from astropy.coordinates import SkyCoord
-    have_astLib = False
+    try:
+        import astropy
+        from astropy.coordinates import SkyCoord
+        which = 2
+    except:
+        pass
 import sys;from sys import stdout
+
+def dist_calc(long1, lat1, long2, lat2):
+    phi1 = np.deg2rad(90.0-lat1)
+    phi2 = np.deg2rad(90.0-lat2)
+    theta1 = np.deg2rad(long1)
+    theta2 = np.deg2rad(long2)
+    cosn = (np.sin(phi1)*np.sin(phi2)*np.cos(theta1-theta2) + 
+           np.cos(phi1)*np.cos(phi2))
+    return np.rad2deg(np.arccos(cosn))
 
 
 def astropy_sep (tra1,tdec1,tra2,tdec2):
@@ -50,8 +64,10 @@ def correlate (array1, ra1, dec1, array2, ra2, dec2, dist, \
             if isabs:
                 adist = np.hypot(r1-r2,d1-d2)
             else:
-                adist = astCoords.calcAngSepDeg(r1,d1,r2,d2) if have_astLib \
-                        else astropy_sep(r1,d2,r2,d2)
+                adist = astCoords.calcAngSepDeg(r1,d1,r2,d2) if which==1 \
+                        else astropy_sep(r1,d1,r2,d2) if which==2 \
+                             else dist_calc(r1,d1,r2,d2)
+                        
 
             if adist<dist and abs(radiff)<90.0 and adist>=mindist:
                 try:
