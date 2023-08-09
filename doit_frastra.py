f66c9d1c7e1e9208058493cdda4cb4cb1c0f3a14
Neal Jackson <neal.jackson@manchester.ac.uk>
2023-08-09 17:33:07 +0100

    Add files via upload

--- doit_frastra.py
+++ doit_frastra.py
@@ -1,4 +1,6 @@
 import os,sys,numpy as np
+os.system('source /home/njj/.cshrc')
+os.system('source /aips/LOGIN.CSH')
 os.system('sed "s/source[i]/sources[i]/g" proc_pmkmap.py >proc_pmkmap1.py')
 os.system('grep sources proc_pmkmap1.py')
 os.system('which parseltongue')
