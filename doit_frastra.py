037d6b6c611c5029012b95a57161db9c43b559ff
Neal Jackson <neal.jackson@manchester.ac.uk>
2023-08-11 18:00:39 +0100

    Add files via upload

--- doit_frastra.py
+++ doit_frastra.py
@@ -5,6 +5,7 @@ import os,sys,numpy as np
 #os.system('grep sources proc_pmkmap1.py')
 #os.system('which parseltongue')
 #os.system('parseltongue proc_pmkmap1.py ../newdata/44059320F.fits')
-os.system('wget https://www.dropbox.com/scl/fi/8ncyaeecr4j9noz8i53xh/labdem.pdf?rlkey=9nar5f0sxvk13mtvp4ebxf0y9&dl=0')
+os.system('rm labdem.pdf')
+os.system('wget https://www.dropbox.com/scl/fi/8ncyaeecr4j9noz8i53xh/labdem.pdf')
 os.system('mv -f labdem.pdf /home/njj/public_html/labdem.pdf')
 os.system('rm doit_frastra.py')
